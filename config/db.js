5c906e3c0e1914c1130962e09edf4955d410e5bc
Ilham Akbar <lorddestiny2@gmail.com>
2019-05-28 00:49:24 +0700

    evento2

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const dbURI =
+  "mongodb+srv://evento:zomboogie987_@cluster0-cjtmx.mongodb.net/test?retryWrites=true";
+
+const options = {
+  reconnectTries: Number.MAX_VALUE,
+  poolSize: 10
+};
+
+mongoose.connect(dbURI, options).then(
+  () => {
+    console.log("Database connection established!");
+  },
+  err => {
+    console.log("Error connecting Database instance due to: ", err);
+  }
+);
+
+// require any models
+
+require("../models/Task");
